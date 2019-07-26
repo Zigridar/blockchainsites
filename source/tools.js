da16cb796d8090b8298db7527dcc1cb2ae5590fe
Ubuntu <ubuntu@ip-172-31-2-2.ec2.internal>
2019-07-26 14:07:28 +0000

    first test

--- source/tools.js
+++ source/tools.js
@@ -1,8 +1,9 @@
 'use strict';
 
-//browserify --debug index.js > ../js/index.js
-
 const utils = require("./utils");
 const tools = require("./tools");
 const $ = require('jquery');
 
+$('#formRPC').submit(e => {
+    alert('on submit')
+});
