62eb5eb2fd6d23b022b50c6ac172964b4cd75b9b
Ubuntu <ubuntu@ip-172-31-2-2.ec2.internal>
2019-07-26 13:10:21 +0000

    rpc template

--- source/utils.js
+++ source/utils.js
@@ -1,1 +1,89 @@
 'use strict';
+
+const $ = require('jquery');
+const zlib = require('zlib');
+const g_crypto = require('crypto');
+const bip65 = require('bip65');
+const bitcoin = require('bitcoinjs-lib');
+const fetch = require('node-fetch');
+
+const networks = {
+  'tBTC' : {
+      url: 'http://195.154.113.90:18332', 
+      user: 'rpc_bts_test', 
+      password: 'rpc_btc_password_test', 
+      name: 'Bitcoin', 
+      NETWORK: bitcoin.networks.testnet
+  },
+};
+
+function sendRPC(method, params, network = "tBTC")
+{
+  const headers = {
+      'Content-Type': 'text/plain', 
+      'Authorization': 'Basic ' + new Buffer(networks[network].user + ':' + networks[network].password).toString('base64')
+  }
+
+  const body = '{"jsonrpc": "1.0", "id":"curltest", "method": "'+method+'", "params": '+params+' }';
+  
+  return fetch(networks[network].url, {
+        method: 'post', 
+        headers: headers,
+        body: body})
+        .then(res => res.json());   
+    
+}
+
+exports.importaddress = function(address, label = "", network = "tBTC")
+{
+    return sendRPC('importaddress', '["'+address+'", "'+label+'", false]', network);
+}
+
+exports.broadcast = function(hex, network = "tBTC")
+{
+    return sendRPC('sendrawtransaction', '["'+hex+'"]', network);
+}
+
+exports.getrawtransaction = function(txid, network = "tBTC")
+{
+    return sendRPC('getrawtransaction', '["'+txid+'", true]', network);
+}
+
+exports.listsinceblock = function(hash, network = "tBTC")
+{
+    return sendRPC('listsinceblock', '["'+hash+'", 1, true]', network);
+}
+
+exports.unspents = function(address = "", conf = 0, maxconf = 9999999, network = "tBTC")
+{
+    const filter = address.length ? ', ["'+address+'"]' : "";
+
+    return sendRPC('listunspent', '['+conf+', '+maxconf+filter+']', network);
+}
+
+exports.height = function(network = "tBTC")
+{
+    return sendRPC('getblockcount', '[]', network);
+}
+
+exports.getblockhash = function(height, network = "tBTC")
+{
+  const headers = {
+      'Content-Type': 'text/plain', 
+      'Authorization': 'Basic ' + new Buffer(networks[network].user + ':' + networks[network].password).toString('base64')
+  }
+    
+  return fetch(networks[network].url, {
+      method: 'post', 
+      headers: headers,
+      body: '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockhash", "params": ['+height+'] }'})
+      .then(res => res.json()); 
+      
+    return sendRPC('getblockhash', '['+height+']', network);
+}
+
+exports.Hash160 = function(str)
+{
+    const buffer = str.length % 2 != 0 ? Buffer.from(str) : Buffer.from(str, "hex");
+    return g_crypto.createHash("ripemd160").update(buffer).digest('hex')
+}
