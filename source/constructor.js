aa476823d0686f841dd0b91ce011aa1908723ab3
Zigridar <safonenko.max@yandex.ru>
2019-08-14 03:39:36 +0500

    CodeMirror

--- source/constructor.js
+++ source/constructor.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const editor = CodeMirror(document.getElementById("codeeditor"), {
+  mode: "text/html",
+  lineNumbers: true,
+  matchBrackets: true,
+  theme: "eclipse",
+  extraKeys: {"Ctrl-Space": "autocomplete"},
+  autocorrect: true,
+  spellcheck: true
+});
