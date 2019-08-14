44d8e8927839b8504889de1533ff1e4207d2f14d
Zigridar <safonenko.max@yandex.ru>
2019-08-15 03:21:50 +0500

    SummerNote

--- source/constructor.js
+++ source/constructor.js
@@ -1,11 +1,33 @@
 'use strict';
 
-const editor = CodeMirror(document.getElementById("codeeditor"), {
-  mode: "text/html",
-  lineNumbers: true,
-  matchBrackets: true,
-  theme: "eclipse",
-  extraKeys: {"Ctrl-Space": "autocomplete"},
-  autocorrect: true,
-  spellcheck: true
+$(document).ready(function() {
+
+  $('#codeeditor').summernote({
+    toolbar: [
+      ['style', ['bold', 'italic', 'underline', 'clear']],
+      ['font', ['strikethrough', 'superscript', 'subscript']],
+      ['fontsize', ['fontname', 'fontsize']],
+      ['color', ['color', 'bac']],
+      ['para', ['ul', 'ol', 'style', 'paragraph']],
+      ['height', ['height']],
+      ['insert', ['picture', 'link', 'table', 'hr']],
+      ['view', ['fullscreen', 'codeview']]
+    ],
+    fontNames: [
+      'Arial',
+      'Courier New',
+    ],
+    minHeight: 150,
+    height: 200,
+    focus: true,
+    codemirror: {
+      mode: "text/html",
+      lineNumbers: true,
+      matchBrackets: true,
+      theme: "eclipse",
+      extraKeys: {"Ctrl-Space": "autocomplete"},
+      autocorrect: true,
+      spellcheck: true
+  }
+  });
 });
