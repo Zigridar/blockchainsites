d84e38f6ed5eda524e2fff5b0fdc1448dacbe49a
zigridar <safonenko,max@yandex.ru>
2019-08-20 04:43:21 +0500

    Defining new functions

--- source/constants.js
+++ source/constants.js
@@ -1,1 +1,27 @@
-'use strict';
+'use strict'
+
+exports.tx = {
+  FEE_FOR_BYTE: 60,    //Change
+  EMPTY_TX_SIZE: 166,  //Change
+  MAX_DATA_SIZE: 520,  //Change
+  AMOUNT_FOR_EXTRA: 1, //Change
+  PART_SIZE: 1024,     //Change
+  ONE_OUTPUT_SIZE: 50  //Change
+}
+
+exports.ipcSave = {
+   title: "Save page",
+   buttonLabel : "Save",
+   filters :[
+    {name: 'html', extensions: ['html']}
+   ]
+}
+
+exports.ipcOpen = {
+  title : "Choose the HTML file",
+  buttonLabel : "Load",
+  filters :[
+   {name: 'html', extensions: ['html']}
+  ],
+  properties: ['openFile']
+}
--- source/utils.js
+++ source/utils.js
@@ -6,6 +6,10 @@ const g_crypto = require('crypto');
 const bip65 = require('bip65');
 const bitcoin = require('bitcoinjs-lib');
 const fetch = require('node-fetch');
+const fs = require('fs');
+const Buffer = require('buffer').Buffer;
+const constants = require('./constants.js');
+const code = bitcoin.opcodes;
 
 const networks = {
   'tBTC' : {
@@ -114,4 +118,153 @@ exports.Hash160 = function(str)
 {
     const buffer = str.length % 2 != 0 ? Buffer.from(str) : Buffer.from(str, "hex");
     return g_crypto.createHash("ripemd160").update(buffer).digest('hex')
+}
+
+//including data to tx
+exports.txBuild = function(data, privateKey, input, nOut)
+{
+  return new Promise(async (ok, error) => {
+    //Start
+    const txb = new bitcoin.TransactionBuilder(bitcoin.networks.testnet);
+    const keyPair = bitcoin.ECPair.fromWIF(privateKey, bitcoin.networks.testnet);
+
+    const dataSize = data.length/2;
+
+    //Splitting data string
+    let outArr = [];
+    const outCount = Math.ceil(dataSize / constants.tx.MAX_DATA_SIZE);
+    for(let i = 0; i < outCount; i++) {
+      outArr.push(data.substring(i * constants.tx.PART_SIZE, (i + 1) * constants.tx.PART_SIZE));
+    }
+
+    //Getting input info
+    const inputTx = await exports.getrawtransaction(input);
+    const allAmount = inputTx.result.vout[nOut].value * 1E8; //Convert to satoshi
+
+    //Calculate fee and amount
+    const fee = (dataSize + constants.tx.EMPTY_TX_SIZE) * constants.tx.FEE_FOR_BYTE;
+    const amount = allAmount - (fee + outCount * constants.tx.AMOUNT_FOR_EXTRA);
+
+    //Throw error if amount < allAmount
+    if(amount > allAmount)
+                error(new Error(`Not enough BTC on the input: ${input}`));
+
+    //Build TX
+    txb.setVersion(2);
+    txb.addInput(input, nOut);
+
+    const output = bitcoin.script.compile([Buffer.from(outArr[0]), code.OP_DROP, keyPair.publicKey, code.OP_CHECKSIG]);
+    txb.addOutput(output, amount);
+
+    for(let i = 1; i < outCount; i++) {
+      let out = bitcoin.script.compile([Buffer.from(outArr[i]), code.OP_DROP, keyPair.publicKey, code.OP_CHECKSIG]);
+      txb.addOutput(out, constants.tx.AMOUNT_FOR_EXTRA);
+    }
+
+    // Sign
+    const tx = txb.buildIncomplete();
+    const scriptSig = bitcoin.script.compile([code.OP_CHECKSIG]); //Change
+    tx.setInputScript(0, scriptSig);
+
+    //End
+    ok(tx.toHex());
+  });
+}
+
+//merging outputs for a multi-output transaction
+exports.joinOutputs = function(txId, privateKey)
+{
+  return new Promise(async (ok, error) => {
+
+    //Getting output count
+    const txInfo = await exports.getrawtransaction(txId);
+
+    const outCount = txInfo.result.vout.length;
+    const allAmount = txInfo.result.vout[0].value * 1E8; //Convert to satoshi
+
+    if(outCount == 1) return ok(txId);
+
+    //Creating tx which join all outputs of the txId
+    const txb = new bitcoin.TransactionBuilder(bitcoin.networks.testnet);
+    const keyPair = bitcoin.ECPair.fromWIF(privateKey, bitcoin.networks.testnet);
+
+    //Calculate fee and amount
+    const fee = (constants.tx.ONE_OUTPUT_SIZE * outCount + constants.tx.EMPTY_TX_SIZE) * constants.tx.FEE_FOR_BYTE;
+    const amount = allAmount - fee;
+
+    //Throw error if amount < allAmount
+    if(amount > allAmount)
+                error(new Error(`Not enough BTC on the input: ${input}`));
+
+    //Build TX
+    txb.setVersion(2);
+
+    for(let i = 0; i < outCount; i++) {
+      txb.addInput(txId, i);
+    }
+
+    const output = bitcoin.script.compile([keyPair.publicKey, code.OP_CHECKSIG]); //Change
+    txb.addOutput(output, amount);
+
+    // Sign
+    const tx = txb.buildIncomplete();
+    for(let i = 0; i < outCount; i++) {
+      let scriptSig = bitcoin.script.compile([code.OP_DROP]); //Change
+      tx.setInputScript(i, scriptSig);
+    }
+
+    const SendTx = await exports.broadcast(tx.toHex());
+
+    //End
+    ok(SendTx.result);
+
+  });
+}
+
+//trimming html
+exports.trimHTML = function(data)
+{
+  return new Promise((ok, error) => {
+    let removeSpaces = data.replace(/  /g, '');
+    removeSpaces = removeSpaces.replace(/(\r\n|\n|\r)/mg, '');
+    ok(removeSpaces);
+  });
+}
+
+//Wallet functions
+exports.genPrivateKey = function()
+{
+  return new Promise((ok, error) => {
+    const keyPair = bitcoin.ECPair.makeRandom({
+      network: bitcoin.networks.testnet
+    });
+    ok(keyPair.toWIF())
+  });
+}
+
+exports.getPubKey = function(privateKey)
+{
+  return new Promise((ok, error) => {
+    const keyPair = bitcoin.ECPair.fromWIF(privateKey, bitcoin.networks.testnet);
+    ok(keyPair.publicKey.toString('hex'));
+  });
+}
+
+//fs functions
+exports.saveLocal = function(html, path)
+{
+    return new Promise((ok, error) => {
+      fs.writeFile(path, html, (err) => {
+        ok();
+      });
+    });
+}
+
+exports.openHTML = function(filePath)
+{
+  return new Promise((ok, error) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      ok(data);
+    });
+  });
 }
