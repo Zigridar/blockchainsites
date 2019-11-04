f69ba0eb4ca93fd99f2f316f538c9de98c382779
zigridar <safonenko.max@yandex.ru>
2019-11-04 18:49:21 +0500

    simple-peer

--- source/navigation.js
+++ source/navigation.js
@@ -4,11 +4,10 @@ const utils = require("./utils");
 const io = require('socket.io-client');
 const blockchaindata = require('blockchaindata-lib');
 const $ = require('jquery');
+const Peer = require('simple-peer');
 const ipcRender = electron.ipcRenderer;
 
-// const url = 'https://desolate-brook-88028.herokuapp.com';
 const url = 'http://144.76.71.116:3000/';
-// const url = 'http://127.0.0.1:3000/';
 
 (async () => {
   let status = await utils.nodeStatus();
@@ -51,17 +50,15 @@ const url = 'http://144.76.71.116:3000/';
 
     $('#statusbar').css('display', 'block');
     //connect to full-node
-    let connect = new connectionToFull();
+    let connect = connectionToFull();
 
     $('#peer_status').addClass('badge-danger');
     $('#peer_status').html('No connection to socket-server');
 
-    // setInterval(function () {
-    //   console.log(connect.peer.connectionState);
-    // }, 3000);
-
     //connection constructor
     function connectionToFull() {
+
+      let obj = {};
 
       const socket = io.connect(url, {
         reconnect: false
@@ -70,62 +67,39 @@ const url = 'http://144.76.71.116:3000/';
       //connection state logs
       const timer = setInterval(function () {
         console.log('connect to server: ' + socket.connected);
+        if(socket.connected) {
+          $('#peer_status').removeClass('badge-primary');
+          $('#peer_status').removeClass('badge-danger');
+          $('#peer_status').addClass('badge-warning');
+          $('#peer_status').html('Connect to socket-server');
+        }
+        else {
+          $('#peer_status').removeClass('badge-primary');
+          $('#peer_status').removeClass('badge-danger');
+          $('#peer_status').addClass('badge-warning');
+          $('#peer_status').html('Connecting to socket-server...');
+        }
+
       }, 2000);
 
-      const iceServers = {
-      	iceServers: [
-          {url:'stun:stun.l.google.com:19302'},
-          {url:'stun:stun1.l.google.com:19302'},
-          {url:'stun:stun2.l.google.com:19302'},
-          {url:'stun:stun3.l.google.com:19302'},
-          {url:'stun:stun4.l.google.com:19302'}
-          // {url:'stun:stun.ekiga.net'},
-          // {url:'stun:stun.ideasip.com'},
-          // {url:'stun:stun.schlund.de'},
-          // {url:'stun:stun.voiparound.com'},
-          // {url:'stun:stun.voipbuster.com'},
-          // {url:'stun:stun.voipstunt.com'},
-          // {url:'stun:stun.xten.com'},
-          // {
-          //   url: 'turn:numb.viagenie.ca',
-          //   credential: 'muazkh',
-          //   username: 'webrtc@live.com'
-          // }
-      	]
-      }
-
-      const lowNode = new RTCPeerConnection(iceServers);
-      this.peer = lowNode;
-      const sendChannel = lowNode.createDataChannel('sendChannel');
-
-      lowNode.onicecandidate = iceCandidate;
-      lowNode.onconnectionstatechange  = connectionState;
-
-      let localOffer;
-
-      lowNode.createOffer()
-      .then(offer => {
-        lowNode.setLocalDescription(offer);
-      })
-      .then(() => {
-        localOffer = lowNode.localDescription;
+      const lowNode = new Peer({
+        initiator: true,
+        trickle: false
       });
 
-      socket.on('answer', (answer, candidate) => {
-        console.log('answer is received');
-        lowNode.setRemoteDescription(answer)
-        .then(() => {
+      const pc = lowNode._pc;
+      obj.peer = pc
+      obj.Peer = lowNode;
+      pc.onconnectionstatechange = connectionState;
 
-          setTimeout(() => {
-            console.log('add ice');
-            candidate.ice.forEach(item => {
-              lowNode.addIceCandidate(item)
-            })
+      lowNode.on('signal', (offer) => {
+        socket.emit('offer', offer);
+        console.log('offer has been sent');
+      });
 
-          }, 3000);
-
-        });
-
+      socket.on('answer', (answer) => {
+        lowNode.signal(answer);
+        console.log('answer has been received');
         socket.disconnect();
         clearInterval(timer);
         $('#peer_status').html('host connection: connected');
@@ -134,72 +108,42 @@ const url = 'http://144.76.71.116:3000/';
         $('#peer_status').removeClass('badge-danger');
       });
 
+      socket.on('disconnect', () => {
+        setTimeout(function () {
+          if(connect.peer.connectionState != 'connected') {
+            lowNode.destroy();
+            connect = connectionToFull();
+            browser.getSrc = getSrc;
+            console.log('reconnect');
+            clearInterval(timer);
+          }
+        }, 10000);
+      });
+
         getSrc = async function(url) {
         return new Promise(ok => {
-            if(lowNode.connectionState == 'connected') {
-              sendChannel.send(url);
-              sendChannel.onmessage = e => {
+            if(connect.peer.connectionState == 'connected') {
+              connect.Peer._channel.send(url);
+              connect.Peer._channel.onmessage = e => {
                 ok(e.data);
               }
             }
         });
       }
 
-      let state = false;
       function connectionState() {
-        console.log(lowNode.connectionState);
-        // if (lowNode.connectionState == 'connecting') {
-        //   setTimeout(function () {
-        //     if (!state) {
-        //       socket.disconnect();
-        //       lowNode.close();
-        //       connect = new connectionToFull();
-        //     }
-        //   }, 4000);
-        // }
-        if (lowNode.connectionState == 'connected') {
-          state = true;
-        }
-        if(lowNode.connectionState == 'closed' || lowNode.connectionState == 'disconnected' || lowNode.connectionState == 'failed') {
-          connect.peer.close();
-          connect = new connectionToFull();
+        console.log(connect.peer.connectionState);
+        if(connect.peer.connectionState == 'closed' || connect.peer.connectionState == 'disconnected' || connect.peer.connectionState == 'failed') {
+          $('#peer_status').removeClass('badge-warning');
+          $('#peer_status').removeClass('badge-primary');
+          $('#peer_status').addClass('badge-danger');
+          $('#peer_status').html('host disconnected');
+          connect = connectionToFull();
+          console.log('reconnect');
           browser.getSrc = getSrc;
         }
       }
-
-      let candidates = {
-        ice: []
-      }
-
-      function iceCandidate(e) {
-        if(e.candidate) {
-          console.log('get candidate');
-          candidates.ice.push(e.candidate);
-          $('#peer_status').html('Connected to socket-server');
-          $('#peer_status').addClass('badge-warning');
-          $('#peer_status').removeClass('badge-primary');
-          $('#peer_status').removeClass('badge-danger');
-        }
-        else {
-          socket.emit('offer', localOffer, candidates);
-          console.log('offer and candidates have sent');
-        }
-      }
-
-      //reconnecting to server
-      socket.on('disconnect', () => {
-        socket.disconnect();
-        setTimeout(function () {
-          if(lowNode.connectionState != 'connected') {
-            lowNode.close();
-            connect = new connectionToFull();
-            browser.getSrc = getSrc;
-            console.log('reconnect');
-            clearInterval(timer);
-          }
-        }, 20000);
-      });
-
+      return obj;
     }
   }
 
--- source/p2p.js
+++ source/p2p.js
@@ -4,17 +4,14 @@ const utils = require('./utils.js')
 const io = require('socket.io-client');
 const blockchaindata = require('blockchaindata-lib');
 const $ = require('jquery');
+const Peer = require('simple-peer');
 
-// const url = 'https://desolate-brook-88028.herokuapp.com';
 const url = 'http://144.76.71.116:3000/';
-// const url = 'http://127.0.0.1:3000/';
 const MAX_PEER_CONNECTIONS = 10;
 
 console.log('I`m full');
 
   let connections = [];
-  let localAnswer;
-  let targetID;
 
   setInterval(function () {
     connections.forEach(item => {
@@ -24,17 +21,9 @@ console.log('I`m full');
 
   $('#peer_status').html('Connected peers: ' + connections.length);
   $('#statusbar').css('display', 'block');
-  // setInterval(function () {
-  //   connections.forEach(item => {
-  //     console.log(item.peer.connectionState);
-  //   });
-  // }, 3000);
-
-  //connection status to socket-server
-  let connectStatus;
 
   //new connection to socket-server
-  let server = new serverConnect();
+  let server = serverConnect();
 
   //connection state logs
   const timer = setInterval(function () {
@@ -43,129 +32,89 @@ console.log('I`m full');
 
   //socket-server connection constructor
   function serverConnect() {
+    let obj = {};
+
     const socket = io.connect(url, {
       reconnect: false
     });
-    connectStatus = true;
+    obj.socket = socket;
+
     socket.emit('full-node');
-    socket.on('offer', (offer, candidate, id) => {
+    socket.on('offer', (offer, id) => {
       console.log('offer is received');
-      connections.push(new lowPeer(socket, offer, candidate, id));
-      console.log(connections);
+      connections.push(lowPeer(socket, offer, id));
+      console.log('connections count: ' + connections.length);
       $('#peer_status').html('Connected peers: ' + connections.length);
     });
-    this.socket = socket;
 
     //reconnecting to server
     socket.on('disconnect', () => {
       if(connections.length < MAX_PEER_CONNECTIONS) {
         console.log('reconnect');
         socket.disconnect();
-        server = new serverConnect();
+        server = serverConnect();
       }
     });
-
-
+    return obj;
   }
 
   //p2p constructor
-  function lowPeer(socket, offer, candidate, id) {
+  function lowPeer(socket, offer, id) {
 
-    const iceServers = {
-    	iceServers: [
-        {url:'stun:stun.l.google.com:19302'},
-        {url:'stun:stun1.l.google.com:19302'},
-        {url:'stun:stun2.l.google.com:19302'},
-        {url:'stun:stun3.l.google.com:19302'},
-        {url:'stun:stun4.l.google.com:19302'}
-        // {url:'stun:stun.ekiga.net'},
-        // {url:'stun:stun.ideasip.com'},
-        // {url:'stun:stun.schlund.de'},
-        // {url:'stun:stun.voiparound.com'},
-        // {url:'stun:stun.voipbuster.com'},
-        // {url:'stun:stun.voipstunt.com'},
-        // {url:'stun:stun.xten.com'},
-        // {
-        //   url: 'turn:numb.viagenie.ca',
-        //   credential: 'muazkh',
-        //   username: 'webrtc@live.com'
-        // }
-    	]
-    }
+    let obj = {};
 
-    const fullNode = new RTCPeerConnection(iceServers);
+    const fullNode = new Peer({
+      initiator: false,
+      trickle: false
+    });
+
+    const pc = fullNode._pc;
+    obj.peer = pc;
+
+    fullNode.signal(offer);
+
+    fullNode.on('signal', (answer) => {
+      socket.emit('answer', answer, id);
+      console.log('answer has been sent');
+      if(connections.length > (MAX_PEER_CONNECTIONS - 1)) {
+        socket.removeAllListeners();
+        socket.disconnect();
+      }
+    });
 
     //callback function
-    fullNode.ondatachannel = dataChannel;
-    fullNode.onconnectionstatechange  = connectionState;
+    pc.onconnectionstatechange = connectionState;
+    pc.ondatachannel = dataChannel;
 
-    let candidates = {
-      ice: []
+    function connectionState(e) {
+      const peer = e.target;
+      if(peer.connectionState == 'closed' || peer.connectionState == 'disconnected' || peer.connectionState == 'failed') {
+        connections.forEach((item, i) => {
+          if(item.peer == e.target) {
+            connections.splice(i, 1);
+            return;
+          }
+        });
+        checkConnections();
+      }
+      console.log(connections);
+      $('#peer_status').html('Connected peers: ' + connections.length);
     }
 
-    fullNode.onicecandidate = function(e) {
-      if (e.candidate) {
-        console.log('get candidate');
-        candidates.ice.push(e.candidate);
-        socket.emit('answer', localAnswer, candidates, targetID);
-        console.log('answer and candidates have sent');
-        if(connections.length > (MAX_PEER_CONNECTIONS - 1)) {
-          socket.removeAllListeners();
-          socket.disconnect();
-          connectStatus = false;
-        }
+    function dataChannel(event) {
+      const receiveChannel = event.channel;
+      receiveChannel.onmessage = async(e) => {
+        let url = e.data.toString();
+        const ret = await blockchaindata.GetObjectFromBlockchain(url.substr(7));
+        url = "data:text/html;base64," + ret.base64;
+        receiveChannel.send(url);
       }
     }
-
-    fullNode.setRemoteDescription(offer)
-    .then(() => fullNode.createAnswer())
-    .then(answer => fullNode.setLocalDescription(answer))
-    .then(() => {
-
-      setTimeout(function () {
-        console.log('add ice');
-        candidate.ice.forEach(item => {
-          fullNode.addIceCandidate(item);
-        });
-      }, 3000);
-
-      localAnswer = fullNode.localDescription;
-      targetID = id;
-    });
-    this.peer = fullNode;
+    return obj;
   }
 
-  //getting data
-  function dataChannel(event) {
-    const receiveChannel = event.channel;
-    receiveChannel.onmessage = async (e) => {
-      let url = e.data.toString();
-      const ret = await blockchaindata.GetObjectFromBlockchain(url.substr(7));
-      url = "data:text/html;base64," + ret.base64;
-      receiveChannel.send(url);
-    }
-  }
-
-  //clean disconnected
-  function connectionState(e) {
-    const peer = e.target
-    if(peer.connectionState == 'closed' || peer.connectionState == 'disconnected' || peer.connectionState == 'failed' ) {
-      connections.forEach((item, i) => {
-        if(item.peer == e.target) {
-          item.peer.close();
-          connections.splice(i, 1);
-          return;
-        }
-      });
-      checkConnections();
-    }
-    console.log(connections);
-    $('#peer_status').html('Connected peers: ' + connections.length);
-  }
-
-  //reconnect socket-server
   function checkConnections() {
-    if(!connectStatus && (connections.length < MAX_PEER_CONNECTIONS)) {
-      server = new serverConnect();
+    if((connections.length < MAX_PEER_CONNECTIONS) && !(server.socket.connected)) {
+      server = serverConnect();
     }
   }
